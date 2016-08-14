aea0c697ff3212767eeff0a247186bdba060ba10
Andrii.kicha <acousticdesk@gmail.com>
2016-08-14 23:28:36 +0300

    Added chart fixes and animations

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 require.config({
     paths: {
-        'd3': './../bower_components/d3/d3'
+        'd3': './../bower_components/d3/d3',
+        'jquery': './../bower_components/jquery/dist/jquery.min'
     }
 });
 
 require(['./chart', './data'], function (chart, data) {
-    chart.create('#chart', data);
+    chart.create('#chart', data.usd['1 month'], {
+        width: '100%',
+        height: 265
+    });
 });
